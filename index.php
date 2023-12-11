fb4dca05a227dee855ef5276232f6f5aebcf3a1e
kyozApp <151455520+kyozApp@users.noreply.github.com>
2023-12-11 14:23:26 -0500

    cotizacion
    al fin

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if ($resultProcesador) {
     $optionsProcesador = '<option value="">Error al obtener los productos de procesador</option>';
 }
 
-// Realiza una consulta para obtener los productos de la tabla procesador
+// Realiza una consulta para obtener los productos de la tabla almacenamiento
 $queryAlmacenamiento = "SELECT Id_Almacenamiento, Producto, Precio FROM almacenamiento";
 $resultAlmacenamiento = $conexion->query($queryAlmacenamiento);
 
@@ -34,6 +34,149 @@ if ($resultAlmacenamiento) {
     $optionsAlmacenamiento = '<option value="">Error al obtener los productos de almacenamiento</option>';
 }
 
+// Realiza una consulta para obtener los productos de la tabla tarjeta_de_video
+$queryTarjetaDeVideo = "SELECT Id_TarjetaDeVideo, Producto, Precio FROM tarjeta_de_video";
+$resultTarjetaDeVideo = $conexion->query($queryTarjetaDeVideo);
+
+// Verifica si la consulta fue exitosa para tarjeta_de_video
+if ($resultTarjetaDeVideo) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para tarjeta_de_video
+    $optionsTarjetaDeVideo = '<option value="" selected>Seleccionar una tarjeta de video</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultTarjetaDeVideo->fetch_assoc()) {
+        $optionsTarjetaDeVideo .= '<option value="' . $row['Id_TarjetaDeVideo'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para tarjeta_de_video
+    $optionsTarjetaDeVideo = '<option value="">Error al obtener los productos de tarjeta_de_video</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla fuente_de_poder
+$queryFuenteDePoder = "SELECT Id_FuenteDePoder, Producto, Precio FROM fuente_de_poder";
+$resultFuenteDePoder = $conexion->query($queryFuenteDePoder);
+
+// Verifica si la consulta fue exitosa para fuente_de_poder
+if ($resultFuenteDePoder) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para fuente_de_poder
+    $optionsFuenteDePoder = '<option value="" selected>Seleccionar una fuente de poder</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultFuenteDePoder->fetch_assoc()) {
+        $optionsFuenteDePoder .= '<option value="' . $row['Id_FuenteDePoder'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para fuente_de_poder
+    $optionsFuenteDePoder = '<option value="">Error al obtener los productos de fuente_de_poder</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla cases
+$queryCases = "SELECT Id_Cases, Producto, Precio FROM cases";
+$resultCases = $conexion->query($queryCases);
+
+// Verifica si la consulta fue exitosa para cases
+if ($resultCases) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para cases
+    $optionsCases = '<option value="" selected>Seleccionar un case</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultCases->fetch_assoc()) {
+        $optionsCases .= '<option value="' . $row['Id_Cases'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para cases
+    $optionsCases = '<option value="">Error al obtener los productos de cases</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla monitor
+$queryMonitor = "SELECT Id_Monitor, Producto, Precio FROM monitor";
+$resultMonitor = $conexion->query($queryMonitor);
+
+// Verifica si la consulta fue exitosa para monitor
+if ($resultMonitor) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para monitor
+    $optionsMonitor = '<option value="" selected>Seleccionar un monitor</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultMonitor->fetch_assoc()) {
+        $optionsMonitor .= '<option value="' . $row['Id_Monitor'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para monitor
+    $optionsMonitor = '<option value="">Error al obtener los productos de monitor</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla teclado
+$queryTeclado = "SELECT Id_Teclado, Producto, Precio FROM teclado";
+$resultTeclado = $conexion->query($queryTeclado);
+
+// Verifica si la consulta fue exitosa para teclado
+if ($resultTeclado) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para teclado
+    $optionsTeclado = '<option value="" selected>Seleccionar un teclado</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultTeclado->fetch_assoc()) {
+        $optionsTeclado .= '<option value="' . $row['Id_Teclado'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para teclado
+    $optionsTeclado = '<option value="">Error al obtener los productos de teclado</option>';
+}
+
+// Agregado: Realiza una consulta para obtener los productos de la tabla mouse
+$queryMouse = "SELECT Id_Mouse, Producto, Precio FROM mouse";
+$resultMouse = $conexion->query($queryMouse);
+
+// Agregado: Verifica si la consulta fue exitosa para mouse
+if ($resultMouse) {
+    // Agregado: Crea el elemento de selección con los productos obtenidos de la base de datos para mouse
+    $optionsMouse = '<option value="" selected>Seleccionar un mouse</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultMouse->fetch_assoc()) {
+        $optionsMouse .= '<option value="' . $row['Id_Mouse'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Agregado: Maneja el caso de error en la consulta para mouse
+    $optionsMouse = '<option value="">Error al obtener los productos de mouse</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla audifono
+$queryAudifono = "SELECT Id_Audifono, Producto, Precio FROM audifono";
+$resultAudifono = $conexion->query($queryAudifono);
+
+// Verifica si la consulta fue exitosa para audífonos
+if ($resultAudifono) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para audífonos
+    $optionsAudifono = '<option value="" selected>Seleccionar un audífono</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultAudifono->fetch_assoc()) {
+        $optionsAudifono .= '<option value="' . $row['Id_Audifono'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para audífonos
+    $optionsAudifono = '<option value="">Error al obtener los productos de audífonos</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla accesorio
+$queryAccesorio = "SELECT Id_Accesorio, Producto, Precio FROM accesorio";
+$resultAccesorio = $conexion->query($queryAccesorio);
+
+// Verifica si la consulta fue exitosa para accesorios
+if ($resultAccesorio) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para accesorios
+    $optionsAccesorio = '<option value="" selected>Seleccionar un accesorio</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultAccesorio->fetch_assoc()) {
+        $optionsAccesorio .= '<option value="' . $row['Id_Accesorio'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para accesorios
+    $optionsAccesorio = '<option value="">Error al obtener los productos de accesorios</option>';
+}
+
+// Realiza una consulta para obtener los productos de la tabla refrigeracion
+$queryRefrigeracion = "SELECT Id_Refrigeracion, Producto, Precio FROM refrigeracion";
+$resultRefrigeracion = $conexion->query($queryRefrigeracion);
+
+// Verifica si la consulta fue exitosa para refrigeración
+if ($resultRefrigeracion) {
+    // Crea el elemento de selección con los productos obtenidos de la base de datos para refrigeración
+    $optionsRefrigeracion = '<option value="" selected>Seleccionar una refrigeración</option>'; // Agregado: opción predeterminada seleccionable
+    while ($row = $resultRefrigeracion->fetch_assoc()) {
+        $optionsRefrigeracion .= '<option value="' . $row['Id_Refrigeracion'] . '" data-precio="' . $row['Precio'] . '">' . $row['Producto'] . '</option>';
+    }
+} else {
+    // Maneja el caso de error en la consulta para refrigeración
+    $optionsRefrigeracion = '<option value="">Error al obtener los productos de refrigeración</option>';
+}
 
 // Cierra la conexión a la base de datos
 $conexion->close();
@@ -56,24 +199,109 @@ $conexion->close();
     </header>
 
     <main class="body-cont">
-        <!-- Agrega un formulario con el select -->
+        <!-- Formulario para el select de procesador -->
         <form>
             <label for="selectProcesador">Procesador:</label>
             <select id="selectProcesador">
                 <?php echo $optionsProcesador; ?>
             </select>
-
-            <!-- Agregado: Span para mostrar el precio -->
             <span id="precioProcesador"></span>
+        </form>
 
+        <!-- Formulario para el select de almacenamiento -->
+        <form>
             <label for="selectAlmacenamiento">Almacenamiento:</label>
             <select id="selectAlmacenamiento">
                 <?php echo $optionsAlmacenamiento; ?>
             </select>
-
-            <!-- Agregado: Span para mostrar el precio -->
             <span id="precioAlmacenamiento"></span>
         </form>
+
+        <!-- Agregado: Formulario para el select de tarjeta_de_video -->
+        <form>
+            <label for="selectTarjetaDeVideo">Tarjeta de video:</label>
+            <select id="selectTarjetaDeVideo">
+                <?php echo $optionsTarjetaDeVideo; ?>
+            </select>
+            <span id="precioTarjetaDeVideo"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de fuente_de_poder -->
+        <form>
+            <label for="selectFuenteDePoder">Fuente de poder:</label>
+            <select id="selectFuenteDePoder">
+                <?php echo $optionsFuenteDePoder; ?>
+            </select>
+            <span id="precioFuenteDePoder"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de cases -->
+        <form>
+            <label for="selectCases">Cases:</label>
+            <select id="selectCases">
+                <?php echo $optionsCases; ?>
+            </select>
+            <span id="precioCases"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de monitor -->
+        <form>
+            <label for="selectMonitor">Monitor:</label>
+            <select id="selectMonitor">
+                <?php echo $optionsMonitor; ?>
+            </select>
+            <span id="precioMonitor"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de teclado -->
+        <form>
+            <label for="selectTeclado">Teclado:</label>
+            <select id="selectTeclado">
+                <?php echo $optionsTeclado; ?>
+            </select>
+            <span id="precioTeclado"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de mouse -->
+        <form>
+            <label for="selectMouse">Mouse:</label>
+            <select id="selectMouse">
+                <?php echo $optionsMouse; ?>
+            </select>
+            <span id="precioMouse"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de audífonos -->
+        <form>
+            <label for="selectAudifono">Audífono:</label>
+            <select id="selectAudifono">
+                <?php echo $optionsAudifono; ?>
+            </select>
+            <span id="precioAudifono"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de accesorios -->
+        <form>
+            <label for="selectAccesorio">Accesorio:</label>
+            <select id="selectAccesorio">
+                <?php echo $optionsAccesorio; ?>
+            </select>
+            <span id="precioAccesorio"></span>
+        </form>
+
+        <!-- Agregado: Formulario para el select de refrigeración -->
+        <form>
+            <label for="selectRefrigeracion">Refrigeración:</label>
+            <select id="selectRefrigeracion">
+                <?php echo $optionsRefrigeracion; ?>
+            </select>
+            <span id="precioRefrigeracion"></span>
+        </form>
+
+        <!-- Agregado: Span para mostrar el precio total -->
+        <span id="precioTotal"></span>
+
+
 
     </main>
 
--- index_producto.js
+++ index_producto.js
@@ -1,37 +1,116 @@
 // index_producto.js
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Obtener elementos de procesador
     var selectProcesador = document.getElementById('selectProcesador');
     var precioProcesador = document.getElementById('precioProcesador');
-
-    // Agrega un evento de cambio al select de procesador
-    selectProcesador.addEventListener('change', function () {
-        // Obtiene la opción seleccionada
-        var selectedOption = selectProcesador.options[selectProcesador.selectedIndex];
-
-        // Muestra la información en la consola
-        console.log('ID Procesador: ' + selectedOption.value);
-        console.log('Producto Procesador: ' + selectedOption.text);
-        console.log('Precio Procesador: $' + selectedOption.dataset.precio);
-
-        // Muestra el precio en el span de procesador
-        precioProcesador.textContent = 'Precio: $' + selectedOption.dataset.precio;
-    });
-
+    // Obtener elementos de almacenamiento
     var selectAlmacenamiento = document.getElementById('selectAlmacenamiento');
     var precioAlmacenamiento = document.getElementById('precioAlmacenamiento');
 
-    // Agrega un evento de cambio al select de almacenamiento
-    selectAlmacenamiento.addEventListener('change', function () {
-        // Obtiene la opción seleccionada
-        var selectedOption = selectAlmacenamiento.options[selectAlmacenamiento.selectedIndex];
+    // Obtener elementos de tarjeta de video
+    var selectTarjetaDeVideo = document.getElementById('selectTarjetaDeVideo');
+    var precioTarjetaDeVideo = document.getElementById('precioTarjetaDeVideo');
 
-        // Muestra la información en la consola
-        console.log('ID Almacenamiento: ' + selectedOption.value);
-        console.log('Producto Almacenamiento: ' + selectedOption.text);
-        console.log('Precio Almacenamiento: $' + selectedOption.dataset.precio);
+    // Obtener elementos de fuente de poder
+    var selectFuenteDePoder = document.getElementById('selectFuenteDePoder');
+    var precioFuenteDePoder = document.getElementById('precioFuenteDePoder');
 
-        // Muestra el precio en el span de almacenamiento
-        precioAlmacenamiento.textContent = 'Precio: $' + selectedOption.dataset.precio;
-    });
+    // Obtener elementos de cases
+    var selectCases = document.getElementById('selectCases');
+    var precioCases = document.getElementById('precioCases');
+
+    // Obtener elementos de monitor
+    var selectMonitor = document.getElementById('selectMonitor');
+    var precioMonitor = document.getElementById('precioMonitor');
+
+    // Obtener elementos de teclado
+    var selectTeclado = document.getElementById('selectTeclado');
+    var precioTeclado = document.getElementById('precioTeclado');
+
+    // Obtener elementos de mouse
+    var selectMouse = document.getElementById('selectMouse');
+    var precioMouse = document.getElementById('precioMouse');
+
+    // Obtener elementos de audífonos
+    var selectAudifono = document.getElementById('selectAudifono');
+    var precioAudifono = document.getElementById('precioAudifono');
+
+    // Obtener elementos de accesorio
+    var selectAccesorio = document.getElementById('selectAccesorio');
+    var precioAccesorio = document.getElementById('precioAccesorio');
+
+    // Obtener elementos de refrigeracion
+    var selectRefrigeracion = document.getElementById('selectRefrigeracion');
+    var precioRefrigeracion = document.getElementById('precioRefrigeracion');
+
+
+    // Agregado: Span para mostrar el precio total
+    var precioTotalSpan = document.getElementById('precioTotal');
+
+    // Agregado: Variables para almacenar los precios de cada select
+    var precios = {
+        'procesador': 0,
+        'almacenamiento': 0,
+        'tarjetaDeVideo': 0,
+        'fuenteDePoder': 0,
+        'cases': 0,
+        'monitor': 0,
+        'teclado': 0,
+        'mouse': 0,
+        'audifono': 0,
+        'accesorio': 0,
+        'refrigeracion': 0
+    };
+
+    // Agregado: Función para actualizar el precio total
+    function actualizarPrecioTotal() {
+        var total = Object.values(precios).reduce((a, b) => a + b, 0);
+
+        // Muestra el precio total en el span
+        precioTotalSpan.textContent = 'Precio Total: $' + total.toFixed(2);
+    }
+
+    // Agregado: Función para actualizar el precio de un select específico
+    function actualizarPrecioSelect(selectId, precio) {
+        precios[selectId] = precio;
+        actualizarPrecioTotal();
+    }
+
+    // Agregado: Función para manejar el cambio en un select específico
+    function manejarCambioSelect(selectId, spanId) {
+        var select = document.getElementById(selectId);
+        var precioSpan = document.getElementById(spanId);
+
+        select.addEventListener('change', function () {
+            var selectedOption = select.options[select.selectedIndex];
+            var precio = parseFloat(selectedOption.dataset.precio);
+
+            // Muestra el precio en el span correspondiente
+            precioSpan.textContent = 'Precio: $' + precio.toFixed(2);
+
+            // Actualiza el precio total
+            actualizarPrecioSelect(selectId, precio);
+
+            // Muestra los detalles por consola
+            console.log('ID ' + selectId + ': ' + selectedOption.value);
+            console.log('Producto ' + selectId + ': ' + selectedOption.text);
+            console.log('Precio ' + selectId + ': $' + precio.toFixed(2));
+        });
+    }
+
+    // Llama a la función para manejar el cambio en cada select
+    manejarCambioSelect('selectProcesador', 'precioProcesador');
+    manejarCambioSelect('selectAlmacenamiento', 'precioAlmacenamiento');
+    manejarCambioSelect('selectTarjetaDeVideo', 'precioTarjetaDeVideo');
+    manejarCambioSelect('selectFuenteDePoder', 'precioFuenteDePoder');
+    manejarCambioSelect('selectCases', 'precioCases');
+    manejarCambioSelect('selectMonitor', 'precioMonitor');
+    manejarCambioSelect('selectTeclado', 'precioTeclado');
+    manejarCambioSelect('selectMouse', 'precioMouse');
+    manejarCambioSelect('selectAudifono', 'precioAudifono');
+    manejarCambioSelect('selectAccesorio', 'precioAccesorio');
+    manejarCambioSelect('selectRefrigeracion', 'precioRefrigeracion');
+
 });
+
